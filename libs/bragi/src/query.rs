e48b839556178dd54377b1ed3cd238edf294e11e
Adrien Matissart <a.matissart@qwantresearch.com>
2020-03-18 18:31:09 +0100

    Revert default weight factor to 0.15 for all objects except Stop
    This is a partial revert of https://github.com/CanalTP/mimirsbrunn/pull/362
    Qwant Maps uses custom weights for POIs, and we noticed important regressions
    with the new coefficients where POIs were too much boosted compared to cities and
    other administrative regions.

--- libs/bragi/src/query.rs
+++ libs/bragi/src/query.rs
@@ -148,7 +148,7 @@ fn build_with_weight<A: Into<Option<f64>>>(factor: A) -> Query {
         FilteredFunction::build_filtered_function(
             Query::build_term("_type", doc_type).build(),
             Function::build_field_value_factor("weight")
-                .with_factor(factor.unwrap_or(0.75))
+                .with_factor(factor.unwrap_or(0.3))
                 .with_missing(0.0)
                 .build(),
             Function::build_weight(weight),
